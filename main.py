4099443585938d2a1cae4c8749e7f8cf6318402c
LAPTOP-312THIB9\tingw <tingwei.liu.19930301@gmail.com>
2021-03-23 21:58:04 +0800

    改為使用py2exe打包

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import pyautogui
-import keyboard
 import pynput
-
-# keyboard.add_hotkey('ctrl+alt', pyautogui.middleClick)
-# keyboard.wait()
-
 
 alt = False
 ctrl = False
@@ -17,27 +12,33 @@ ctrl_list = [
     pynput.keyboard.Key.ctrl_r,
 ]
 
-def on_press(key):
-    global alt, ctrl
-    
-    if key in alt_list:
-        alt = True
-    if key in ctrl_list:
-        ctrl = True
 
-    if alt and ctrl:
-        pyautogui.middleClick()
-        alt = ctrl = False
+def main():
+    def on_press(key):
+        global alt, ctrl
+        
+        if key in alt_list:
+            alt = True
+        if key in ctrl_list:
+            ctrl = True
 
-def on_release(key):
-    global alt, ctrl
-    if key in alt_list:
-        alt = False
-    if key in ctrl_list:
-        ctrl = False
+        if alt and ctrl:
+            pyautogui.middleClick()
+            alt = ctrl = False
 
-with pynput.keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release
-) as listener:
-    listener.join()
+    def on_release(key):
+        global alt, ctrl
+        if key in alt_list:
+            alt = False
+        if key in ctrl_list:
+            ctrl = False
+
+    with pynput.keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release
+    ) as listener:
+        listener.join()
+
+
+if __name__ == '__main__':
+    main()
